d5856d7ca86d32f50f116cfa3ac611e872dabfd6
carlos gizbert <carlos.gizbert@fcamara.com.br>
2022-11-29 00:42:55 -0300

    fix .env provisory

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-export const mongoConnectionString = process.env.MONGO_CONNECTION_STRING || 'mongodb://localhost:27017'
-export const apiPort = process.env.API_PORT || 'mongodb://localhost:27017'
+export const mongoConnectionString = process.env.MONGO_CONNECTION_STRING || 'mongodb+srv://carlosgizbert:ricoem2030@cardapio.pvb6pbs.mongodb.net/?retryWrites=true&w=majority'
+export const apiPort = process.env.API_PORT || '3001'
 export const secret = process.env.SECRET || String(Math.random() ** Math.random())
