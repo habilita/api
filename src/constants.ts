08bc0ac0530ca7c4cf3906d4c63417974f4d79b5
Carlos Gizbert <48734715+carlosgizbert@users.noreply.github.com>
2022-12-05 10:43:20 -0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-export const mongoConnectionString = process.env.MONGO_CONNECTION_STRING || 'mongodb+srv://carlosgizbert:ricoem2030@cardapio.pvb6pbs.mongodb.net/?retryWrites=true&w=majority'
+export const mongoConnectionString = process.env.MONGO_CONNECTION_STRING || ''
 export const apiPort = process.env.API_PORT || '3001'
 export const secret = process.env.SECRET || String(Math.random() ** Math.random())
